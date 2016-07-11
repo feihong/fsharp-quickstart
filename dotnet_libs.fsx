8c4db9143bb592d214e71265c7c4c64808fb4d41
Feihong <feihong.hsu@gmail.com>
2016-07-10 19:30:59 -0500

    Add an example showing how to use .NET libraries

---
+++ dotnet_libs.fsx
@@ -0,0 +1,13 @@
+open System.IO
+open System.Net
+
+let http (url: string) =
+  let req = WebRequest.Create(url)
+  let resp = req.GetResponse()
+  let stream = resp.GetResponseStream()
+  let reader = new StreamReader(stream)
+  let html = reader.ReadToEnd()
+  resp.Close()
+  html
+
+let _ = printfn "%s" (http "http://ipecho.net/plain")
