439d6c091703f1c08508ac3013edd861e10fcdd3
Feihong Hsu <feihong.hsu@gmail.com>
2020-11-07 21:09:55 -0600

    Copy hello.fs into Program.fs

--- Program.fs
+++ Program.fs
@@ -1,8 +1,17 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+let rand = System.Random()
+
+let getRandomHanzi () =
+  rand.Next(0x4e00, 0x9fff + 1)
+  |> Convert.ToChar
+  |> Convert.ToString
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+  printfn "你好世界！\nHere are some random characters:"
+
+  for i = 1 to 10 do
+    printfn "%s" (getRandomHanzi ())
+
+  0 // return an integer exit code
