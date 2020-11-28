4d5a1c5fdce5ae733051b725d810346a9c11022d
Feihong Hsu <feihong.hsu@gmail.com>
2020-11-27 21:31:25 -0600

    Generate table of tracks

--- html_write.fsx
+++ html_write.fsx
@@ -31,13 +31,36 @@ type Tracks = JsonProvider<"""
 // for track in Tracks.GetSamples() do
 //   printfn "%s by %s (%s)" track.Title track.Artist track.Genre
 
+let cellStyle = _style "padding: 0.5rem; border: 1px solid gray;"
+
 let page =
   html [] [
     head [] [
       meta [ _charset "utf-8" ]
     ]
     body [] [
-      h1 [] [ str "你好世界！" ]
+      h1 [] [ str "歌曲" ]
+      table [ _style "border-collapse: collapse" ] [
+        thead [] [
+          tr [] [
+            th [ cellStyle ] [ str "名字" ]
+            th [ cellStyle ] [ str "歌手" ]
+            th [ cellStyle ] [ str "风格" ]
+          ]
+        ]
+        tbody [] [
+          for track in Tracks.GetSamples() ->
+            tr [] [
+              td [ cellStyle ] [
+                a [ _href track.Link; _target "_blank" ] [
+                  str track.Title
+                ]
+              ]
+              td [ cellStyle ] [ str track.Artist ]
+              td [ cellStyle ] [ str track.Genre ]
+            ]
+        ]
+      ]
     ]
   ]
 
