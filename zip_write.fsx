d9c5539ab799804ca2aeee69ec4b62d7c6b58bde
Feihong Hsu <feihong.hsu@gmail.com>
2020-11-28 13:17:14 -0600

    Add globbing to zip example

--- zip_write.fsx
+++ zip_write.fsx
@@ -3,19 +3,31 @@
   Usage:
 
   dotnet fsi zip_write.fsx temp.html temp.json README.md
+  unzip -l temp.zip
 
 *)
 #r "nuget: Fake.IO.Zip"
+#r "nuget: Fake.IO.FileSystem"
 
 open Fake.IO
+open Fake.IO.Globbing.Operators
 
 // First argument is the name of the script itself
-let files =
+let files: string seq =
   match fsi.CommandLineArgs with
-  | args when args.Length > 1 -> args.[1..]
-  | _ -> [|"README.md"|]
+  | args when args.Length > 1 -> args.[1..] |> Array.toSeq
+  | _ -> !! "./*.fsx" |> Seq.cast<string>
 
-printfn "Zipping up these files: %s\n" (files |> String.concat ", ")
+printfn "Zipping up these files:\n"
+for file in files do
+  printfn "%s" file
 
-Zip.createZip "." "temp.zip" "ignored comment" 0 true files
-printfn "Generated temp.zip"
+Zip.createZip
+  "."         // working directory
+  "temp.zip"  // zip file name
+  ""          // ignored comment
+  0           // compression level
+  true        // all files are put at root level
+  files       // sequence of file paths
+
+printfn "\nGenerated temp.zip"
